e1688627a2315473c1cd615d1237684a3c5f1656
Richard Ortiz <38478922+Richardortiz78@users.noreply.github.com>
2022-02-10 23:01:14 -0300

    se culmino el dibujo del cadalso y del ahorcado y se transformo la palabra secreta a guiones

--- dibujar.js
+++ dibujar.js
@@ -1,11 +1,55 @@
-function dibujar() {
-    var canvas = document.getElementById("canvas");
-    var pincel =canvas.getContext("2d");
+var canvas = document.getElementById("canvas");
+var pincel =canvas.getContext("2d");
 
-    pincel.fillStyle= "white";
-    pincel.beginPath(390,1000)
-    pincel.moveTo(370,980)
+function cadalso() {
+    
+
+    pincel.fillStyle= "brown";
+    pincel.fillRect(220,120, 60,08);
+    pincel.fillRect(245,28, 10,100);
+    pincel.fillRect(150,20, 120,08);
+    pincel.fillRect(150,28, 10,20);
+    
+    pincel.beginPath()
+	  pincel.moveTo(200,20);
+	  pincel.lineTo(248,45);
+	  pincel.lineTo(248,55);
+	  pincel.fill();
   }
+  cadalso();
 
-dibujar();
+  function ahorcado(){
+    //cabeza
+    pincel.lineWidth=4;
+    pincel.fillStyle="white";
+    pincel.beginPath();
+    pincel.arc(155,60,10,0,Math.PI*2, true);
+    pincel.stroke();
+    
+    //torso
+    pincel.beginPath();
+    pincel.moveTo(155,71);
+    pincel.lineTo(155,91);
+    pincel.stroke();
 
+    //manos
+    pincel.beginPath();
+    pincel.moveTo(155,71);
+    pincel.lineTo(190,91);
+    pincel.stroke();
+    pincel.moveTo(155,71);
+    pincel.lineTo(126,91);
+    pincel.stroke();
+
+    //piernas
+    pincel.beginPath();
+    pincel.moveTo(155,91);
+    pincel.lineTo(190,111);
+    pincel.stroke();
+    pincel.moveTo(155,91);
+    pincel.lineTo(126,111);
+    pincel.stroke();
+
+    }
+
+    ahorcado()
--- palabras.js
+++ palabras.js
@@ -1,9 +1,26 @@
 var palabras = ["Esfera", "Fosforo", "Lucidez", "Economia", "Estatua", "Bateria", "Codorniz", "Programar", "Leyenda", "Canario"];
-console.log(palabras);
-
-
 var random = Math.floor(Math.random()*palabras.length);
 var escogerPalabra = palabras[random];
+
 console.log(escogerPalabra);
+console.log(escogerPalabra.length)
+
+var palabraConGuiones = escogerPalabra.replace(/./g, " _");
+pincel.fillStyle="white"
+pincel.font="bold italic 24px arial";
+pincel.fillText(palabraConGuiones,20,120)
+
+
+
+
+
+
+
+
+
+
+
+ 
+
 
 
